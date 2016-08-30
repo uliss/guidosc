51f3d9285a62e2f638d704ed21a4ad30a20074eb
Serge Poltavski <serge.poltavski@gmail.com>
2016-08-30 16:01:05 +0300

    ping returns it argument, like echo

--- server/src/ping.js
+++ server/src/ping.js
@@ -1,11 +1,12 @@
 var inherits = require('inherits');
 var utils = require('./utils');
 var mod = require('./module.js');
+var log = utils.log('ping');
 
 function Ping(app_global) {
     mod.Module.call(this, app_global, 'ping');
-    this.addCommand("ping", "ping server", function() {
-        return "pong";
+    this.addCommand("ping", "ping server", function(msg) {
+        return msg;
     });
 }
 
